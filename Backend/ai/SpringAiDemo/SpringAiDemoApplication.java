30e81c1c6bccbcd45498d4cf813b4393e22e506f
Manisha S <asmanisha074@gmail.com>
2025-07-29 21:35:01 +0530

    Update SpringAiDemoApplication.java

--- Backend/ai/SpringAiDemo/SpringAiDemoApplication.java
+++ Backend/ai/SpringAiDemo/SpringAiDemoApplication.java
@@ -1,4 +1,4 @@
-package com.ai.SpringAiDemo;
+package Backend.ai.SpringAiDemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
